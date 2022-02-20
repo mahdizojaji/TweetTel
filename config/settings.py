73df4791d07fb440a7893286c87634c22935b877
Mahdi Zojaji <mmahdizojajim@gmail.com>
2022-02-21 00:41:20 +0330

    Create twitter django app

--- config/settings.py
+++ config/settings.py
@@ -32,12 +32,15 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv(delimiter=' '))
 # Application definition
 
 INSTALLED_APPS = [
+    # Django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Local apps
+    'twitter.apps.TwitterConfig',
 ]
 
 MIDDLEWARE = [
