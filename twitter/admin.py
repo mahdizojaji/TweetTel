2564f27cd1c330353b3e58ed6e1f5c160eb00662
Mahdi Zojaji <mmahdizojajim@gmail.com>
2022-02-21 06:41:15 +0330

    Add TargetedUsers to django-admin

--- twitter/admin.py
+++ twitter/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import TargetUser
+
+
+@admin.register(TargetUser)
+class TargetUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'uuid', 'twitter_id', 'username', 'name', 'created_at', 'updated_at')
+    fields = ('id', 'uuid', 'twitter_id', 'username', 'name', 'created_at', 'updated_at')
+    readonly_fields = ('id', 'uuid', 'created_at', 'updated_at')
+
+    def save_model(self, request, obj, form, change):
+        if isinstance(obj.username, str):
+            obj.username = obj.username.lower()
+        super().save_model(request, obj, form, change)
